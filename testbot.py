16505598712755c7c2a3bde241de3e96f3fc82e3
Gilcare <gilcare.com@gmail.com>
2026-01-11 23:02:17 +0000

    testbot.py

--- testbot.py
+++ testbot.py
@@ -96,7 +96,7 @@ async def create_scheduling_journey(server: p.Server, agent: p.Agent) -> p.Journ
    return journey
 
 # Set Up Parlant Server To Run Agent
-async def initialize_parlant():
+async def initialize_parlant() -> None:
     async with p.Server(nlp_service=p.NLPServices.litellm) as server:
         agent = await server.create_agent(
            name = "Kyma",
