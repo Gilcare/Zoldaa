429c58c04669a5fa89ba2ede6fa059b54af11a96
Gilcare <gilcare.com@gmail.com>
2026-01-11 12:19:29 +0000

    testbot.py

--- testbot.py
+++ testbot.py
@@ -12,6 +12,11 @@ from dotenv import load_dotenv
 #LITELLM_API_KEY = st.secrets.litellm_api_key
 #LITELLM_PROVIDER_MODEL_NAME="huggingface/deepseek-ai/DeepSeek-R1-0528-Qwen3-8B"
 
+from parlant import Chatbot
+
+bot = Chatbot(
+    embedding_model="sentence-transformers/all-MiniLM-L6-v2"
+)
 
 
 
