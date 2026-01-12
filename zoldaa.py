f570ed4195a09b0e50ff71b84fac48f5edf3c609
Gilcare <gilcare.com@gmail.com>
2026-01-12 22:25:56 +0000

    Update zoldaa.py

--- zoldaa.py
+++ zoldaa.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 from pymongo import MongoClient
 from threading import Thread
 from transformers import pipeline, TextIteratorStreamer
-from testbot import initialize_parlant, get_response
+
 
 
 
