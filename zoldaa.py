2f41a467e63d70f5a81cbbe47aec399cb1495d0d
Gilcare <gilcare.com@gmail.com>
2026-01-17 11:50:46 +0000

    Update zoldaa.py

--- zoldaa.py
+++ zoldaa.py
@@ -390,10 +390,10 @@ def landing_page():
             st.session_state.messages.append({"role": "assistant", "content": full_response})
 
         elif app == "🌏 About":
-            st.image("thrive_about_1.webp")
+            st.image("group_of_women.png")
             st.caption ("_❤️AI-Driven Healthcare, Accessible and Affordable for Every lady_")
             st.write("Every lady deserves access to quality healthcare, no matter where she lives. We're on a mission to make affordable, world-class care available to women & girls across Africa and beyond. It's time to prioritize women's health, globally.")
-            st.image("iron_lady.avif")
+            st.image("iron_lady.png")
             st.caption ("_...built with 💜 ZOLDAA_")
             st.write("Together, we can make women's health a priority, and ensure that every woman is empowered with the knowledge and resources she needs to thrive")
 
@@ -405,6 +405,8 @@ def landing_page():
             st.session_state.logged_in = False
             st.rerun()        
 
+if 'logged_in' not in st.session_state:
+        st.session_state.logged_in = False
 
 def main():
     if not st.session_state.logged_in:
@@ -416,3 +418,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
